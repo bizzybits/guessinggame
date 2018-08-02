4fd62bb1db928415a6246d68eedf5fbaf5628ef8
Elizabeth Clarkson <elizabeth.m.clarkson@gmail.com>
2018-08-01 20:57:12 -0700

    function great working

--- guessinggame.js
+++ guessinggame.js
@@ -1,1 +1,20 @@
-dahgag;hd
+
+
+
+      function showGreat() {
+          //alert("Great Job!");
+         var great = document.getElementById("great");
+         console.log(great)
+         great.innerHTML += '<span class="large">Great Job!</span>';
+         console.log(great)
+       }
+
+       function showNope() {
+          alert("Nope!");
+        // var wrongContainer = document.getElementById("wrong");
+        // wrongContainer.innerHTML = "Wrong!";
+       }
+      //function showAnswer(answer) {
+      //  var container = document.getElementById(answer+"-container");
+      //  container.innerHTML += '<span class="large">' + answer + '!</span>';
+    //  }
