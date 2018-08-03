3453a62a7992657a99640cde95ff5bb242a237ea
Elizabeth Clarkson <elizabeth.m.clarkson@gmail.com>
2018-08-02 19:15:49 -0700

    counter and results working

--- guessinggame.js
+++ guessinggame.js
@@ -1,18 +1,22 @@
 
+      var counter = 0
 
 
       function showGreat() {
           //alert("Great Job!");
-         var great = document.getElementById("great");
+         var great = document.getElementById("result");
          console.log(great)
-         great.innerHTML += '<span class="large">Great Job!</span>';
+         great.innerHTML = '<span class="large">Great Job!</span>';
          console.log(great)
+         var numberCorrect = document.getElementById("counter")
+         counter++;
+         numberCorrect.innerHTML = counter ;
        }
 
        function showNope() {
           //alert("Nope!");
-         var nope = document.getElementById("nope");
-         nope.innerHTML += "Nope!";
+         var nope = document.getElementById("result");
+         nope.innerHTML = "Nope!";
        }
       //function showAnswer(answer) {
       //  var container = document.getElementById(answer+"-container");
